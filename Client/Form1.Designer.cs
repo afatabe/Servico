62fd193e7839351232b1a89e0755fc9699af2a42
anderson f antonio <gilafa@hotmail.com>
2016-03-16 10:20:53 -0300

    SERVICO OK

--- Client/Form1.Designer.cs
+++ Client/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(471, 386);
+            this.ClientSize = new System.Drawing.Size(573, 386);
             this.Name = "Form1";
             this.Text = "Cliente WCF";
             this.ResumeLayout(false);
